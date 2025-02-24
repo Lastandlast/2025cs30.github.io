76b49b64806932039e1a748a48262e18f0bf5049
Lastandlast <145151247+Lastandlast@users.noreply.github.com>
2025-02-24 12:17:11 -0600

    drawing with single loops

--- 05 Drawing with single loops/sketch.js
+++ 05 Drawing with single loops/sketch.js
@@ -6,10 +6,58 @@
 // - describe what you did to take this project "above and beyond"
 
 
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+// }
+
+// function draw() {
+//   gradientBackground();
+//   circleLine();
+// }
+
+// function gradientBackground(){
+//   let h = 10;
+//   for(let y = 0; y < height; y += h){
+//     noStroke();
+//     let mappedY = map(y,0,height,0,255);
+//     let reversedY = map (y,0,height,255,0);
+//     fill(mappedY, reversedY, 0);
+//     rect(0, y, width, h);
+    
+//   }
+// }
+
+
+// function circleLine(){
+//   let d = 40;
+//   let y  = height/2;
+//   let xStart = 30;
+//   let xEnd = mouseX;
+
+//   for(let x = xStart; x <= xEnd; x+=d){
+
+//     circle(x, y, d);
+//   }
+// }
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(1);
+  circleLine();
+}
+
+function circleLine(){
+  let d = 40;
+  let y  = height/2;
+  let xStart = 30;
+  let xEnd = mouseX;
+
+  for(let x = xStart; x <= xEnd; x+=d){
+
+    circle(x, y, d);
+  }
 }
