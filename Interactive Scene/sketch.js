e97c43180b7a189841a5d5caeb1a08aa9123d89c
Lastandlast <sy3405046211@gmail.com>
2025-02-26 23:24:47 -0600

    Final

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,29 +1,27 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Interactive Scene
+// Mason
+// 25/02/2025
 
-let x, y;
-let currentBack = 0;
+let x, y; //character position
+let currentBack = 0; //background color variable
 function setup() {
   createCanvas(950, windowHeight);
-  x = width / 2;
+  x = 0; //character x position
   y = 250;
 }
 
 function draw() {
-  setbackground();
-  SUN();
-  HILL();
-  RIVER();
-  Name();
+  setbackground(); //background color
+  SUN(); //sun
+  HILL(); //hill
+  RIVER(); //river
+  Name(); //name
   translate(x, y);
   Character();
 }
 
 function setbackground() {
+  // Change background based on currentBack state
   if (currentBack === 0) {
     background(135, 206, 235);
   } else if (currentBack === 1) {
@@ -36,6 +34,7 @@ function setbackground() {
 }
 
 function keyPressed() {
+  // Move character based on arrow key pressed
   if (keyCode === LEFT_ARROW) {
     x -= 40;
   } else if (keyCode === RIGHT_ARROW) {
@@ -44,6 +43,7 @@ function keyPressed() {
 }
 
 function mousePressed() {
+  // Change background based on middle mouse button pressed
   if (mouseButton === CENTER) {
     currentBack++;
     if (currentBack > 3) {
@@ -53,28 +53,34 @@ function mousePressed() {
 }
 
 function RIVER() {
+  //Draws the river
   fill(18, 82, 140);
   noStroke();
   rect(0, 500, windowWidth, windowHeight / 2);
 }
 function HILL() {
+  //Draws the hills
   fill(133, 108, 9);
   noStroke();
   triangle(-100, windowHeight / 2 + 50, 600, windowHeight / 2 + 50, 200, 75);
   triangle(300, windowHeight / 2 + 50, 1100, windowHeight / 2 + 50, 800, 75);
 }
 function SUN() {
+  //Draws the sun
   fill(255, 204, 51);
   noStroke();
   circle(470, 330, 150);
 }
 
 function Character() {
+  //Draws the character
   stroke(0, 0, 0);
   strokeWeight(4);
   fill(0, 3, 105);
+  //Legs
   ellipse(510, 530, 15, 30);
   ellipse(490, 530, 15, 30);
+  //Body
   circle(500, 500, 50);
   circle(500, 510, 15);
   circle(500, 510, 2);
@@ -83,6 +89,7 @@ function Character() {
 }
 
 function Name() {
+  //Displays name
   fill(255);
   textSize(30);
   text("Mason", 800, 950);
