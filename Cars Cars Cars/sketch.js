89154acd32af2981205e464284b7aa39f7bd6957
Lastandlast <sy3405046211@gmail.com>
2025-03-27 11:24:58 -0600

    03272025

--- Cars Cars Cars/sketch.js
+++ Cars Cars Cars/sketch.js
@@ -8,32 +8,58 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  vechile = new vechile(windowWidth / 2, windowHeight / 2, 1, 1);
 }
 
 function draw() {
   background(220);
   DrawRoad();
+  vechile.display();
+  vechile.changecolor();
 }
 
 
-function DrawRoad(){
+function DrawRoad() {
   rectMode(CENTER)
-  fill(0)
-  rect(0 , height/2, windowWidth*2, 400);
-  
-  
+  fill(0);
+  rect(0, height / 2, windowWidth * 2, 400);
+
+
   for (let i = 0; i < windowWidth; i++) {
-    rect(i*20, height/2, 10, 5);
-    fill(210, 235, 52)
-    }
+    rect(i * 20, height / 2, 10, 5);
+    fill(210, 235, 52);
+  }
 }
 
-class vechile(){
-  rect(x,y, direction, speed){
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.direction = direction;
-    
+class vechile {
+constructor(x, y, direction, speed){
+this.Type = int(random(2)); //0,1}
+this.x = x;
+this.y = y;
+this.speed = speed;
+this.direction = direction;
+this.color = color(random(255), random(255), random(255));
+}
+
+changecolor(){
+  this.color = color(random(255), random(255), random(255));
+}
+display(){
+  fill(this.color);
+  if (this.type === 0) {
+    rect(this.x - 10, this.y, 10, 40);
+    rect(this.x + 10, this.y, 10, 40);
+  } else {
+    rect(this.x, this.y, 40, 30);
   }
+
+  fill(255, 0, 0);
+}
+
+action(){
+  
+}
+move(){
+
+}
 }
