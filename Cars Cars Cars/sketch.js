79070abaf59d5114978ec70e19960a379f65dfbb
Lastandlast <sy3405046211@gmail.com>
2025-04-01 20:24:39 -0600

    01042025

--- Cars Cars Cars/sketch.js
+++ Cars Cars Cars/sketch.js
@@ -5,7 +5,6 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   vechile = new vechile(windowWidth / 2, windowHeight / 2, 1, 1);
@@ -16,14 +15,13 @@ function draw() {
   DrawRoad();
   vechile.display();
   vechile.changecolor();
+  vechile.update();
 }
 
-
 function DrawRoad() {
-  rectMode(CENTER)
+  rectMode(CENTER);
   fill(0);
   rect(0, height / 2, windowWidth * 2, 400);
-
 
   for (let i = 0; i < windowWidth; i++) {
     rect(i * 20, height / 2, 10, 5);
@@ -32,34 +30,36 @@ function DrawRoad() {
 }
 
 class vechile {
-constructor(x, y, direction, speed){
-this.Type = int(random(2)); //0,1}
-this.x = x;
-this.y = y;
-this.speed = speed;
-this.direction = direction;
-this.color = color(random(255), random(255), random(255));
-}
-
-changecolor(){
-  this.color = color(random(255), random(255), random(255));
-}
-display(){
-  fill(this.color);
-  if (this.type === 0) {
-    rect(this.x - 10, this.y, 10, 40);
-    rect(this.x + 10, this.y, 10, 40);
-  } else {
-    rect(this.x, this.y, 40, 30);
+  constructor(x, y, direction, speed) {
+    this.Type = int(random(2)); //0,1}
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.direction = direction;
+    this.color = color(random(255), random(255), random(255));
   }
 
-  fill(255, 0, 0);
-}
+  changecolor() {
+    this.color = color(random(255), random(255), random(255));
+  }
+  display() {
+    fill(this.color);
+    if (this.type === 0) {
+      rect(this.x - 10, this.y, 10, 40);
+      rect(this.x + 10, this.y, 10, 40);
+    } else {
+      rect(this.x, this.y, 40, 30);
+    }
 
-action(){
-  
-}
-move(){
-
-}
+    fill(255, 0, 0);
+  }
+  update() {
+    if (mills - this.timer > this.interval) {
+      this.changecolor();
+      this.timer = mills;
+      this.interval = random(500, 3000);
+    }
+  }
+  action() {}
+  move() {}
 }
--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -6,41 +6,63 @@
 // - describe what you did to take this project "above and beyond"
 
 let rectWidth = 1;
-let noiseScale = 0.01;
+let noiseScale = 0.015;
+let terrianShift = 0;
+
+let Xpeak = 0;
+let Ypeak = 0;
+
+let averageHeight = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
 }
 
-
-function generateTerrain(){
+function generateTerrain() {
   rectMode(CORNERS);
-  for(let x = 0; x < width; x += rectWidth){
-    let NOise = noise(x * noiseScale);
-    let rectHeight = map(NOise, 0, 1, 50, 500);
+
+  let maxHeight = 0;
+  let sumHeight = 0;
+  let count = 0;
+  for (let x = 0; x < width; x += rectWidth) {
+    let NOise = noise(x * noiseScale + terrianShift);
+    let rectHeight = map(NOise, 0, 1, 10, 700);
 
     let x2 = x + rectWidth;
     let y2 = height - rectHeight;
-    rect(x, height, x2, y2);
-  }
-  rectMode(CORNER);
 
-  
+    rect(x, height, x2, y2);
+
+    if (rectHeight > maxHeight) {
+      maxHeight = rectHeight;
+      Xpeak = x;
+      Ypeak = y2;
+    }
+
+    sumHeight += rectHeight;
+    count++;
+  }
+  let averHeight = sumHeight / count;
+  averageHeight = height - averHeight;
+  rectMode(CORNER);
 }
 
-
-
 function draw() {
-
   background(220);
   generateTerrain();
   flag();
-  
- 
+  terrianShift += 0.1;
+  drawaverageline();
 }
 
-function flag(){
+function flag() {
   fill(0);
-  rect(200,200,5,45);
+  rect(Xpeak, Ypeak - 45, 5, 45);
+  fill(255, 0, 0);
+  triangle(Xpeak, Ypeak - 45, Xpeak + 30, Ypeak - 45, Xpeak, Ypeak - 70);
+}
 
+function drawaverageline() {
+  fill(255, 0, 0);
+  strokeWeight(2);
+  rect(0, averageHeight, width, 5);
 }
