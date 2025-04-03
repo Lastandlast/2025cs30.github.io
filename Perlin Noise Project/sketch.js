94613190b2d9aecbaaab843dc8bce30f7d533eec
Lastandlast <sy3405046211@gmail.com>
2025-04-02 23:24:01 -0600

    comments

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -1,47 +1,50 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Perlin Noise Project
+// Mason
+// 02042025
 
+// set width of rectangles, noisescale, and terrianShift speed
 let rectWidth = 1;
 let noiseScale = 0.015;
 let terrianShift = 0;
 
+// Variables to store the highest point of the terrain
 let Xpeak = 0;
 let Ypeak = 0;
 
+// Variable to store the average height of the terrain
 let averageHeight = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function generateTerrain() {
+  // Create the terrain
   rectMode(CORNERS);
 
-  let maxHeight = 0;
-  let sumHeight = 0;
-  let count = 0;
+  let maxHeight = 0; // Set the current maximum height to 0
+  let sumHeight = 0; // Set the current sum of heights to 0
+  let count = 0; // Set counter to calculate average height
   for (let x = 0; x < width; x += rectWidth) {
-    let NOise = noise(x * noiseScale + terrianShift);
+    // Loop through the width of the canvas and draw terrain rectangle
+    let NOise = noise(x * noiseScale + terrianShift); // Calculate perlin noise value
     let rectHeight = map(NOise, 0, 1, 10, 700);
 
     let x2 = x + rectWidth;
     let y2 = height - rectHeight;
 
-    rect(x, height, x2, y2);
+    rect(x, height, x2, y2); // Draw the rectangle
 
     if (rectHeight > maxHeight) {
+      // Check is this the highest point
       maxHeight = rectHeight;
       Xpeak = x;
       Ypeak = y2;
     }
 
-    sumHeight += rectHeight;
+    sumHeight += rectHeight; // add height to sum
     count++;
   }
-  let averHeight = sumHeight / count;
+  let averHeight = sumHeight / count; // calculate average height to y coordinate
   averageHeight = height - averHeight;
   rectMode(CORNER);
 }
@@ -55,13 +58,15 @@ function draw() {
 }
 
 function flag() {
+  // Draw the flag
   fill(0);
   rect(Xpeak, Ypeak - 45, 5, 45);
-  fill(255, 0, 0);
+  fill(255, 0, 0); // flag color
   triangle(Xpeak, Ypeak - 45, Xpeak + 30, Ypeak - 45, Xpeak, Ypeak - 70);
 }
 
 function drawaverageline() {
+  // Draw the average line
   fill(255, 0, 0);
   strokeWeight(2);
   rect(0, averageHeight, width, 5);
