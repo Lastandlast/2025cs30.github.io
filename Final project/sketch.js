87c1c229df05b9abd545d4c341d40a8b4804e7fe
Yao7004906 <145151247+Lastandlast@users.noreply.github.com>
2025-05-23 12:17:28 -0600

    Make mario can move and the background can move smooth

--- Final project/sketch.js
+++ Final project/sketch.js
@@ -9,40 +9,50 @@ let platform;
 let Mariomove = [];
 let currentFrame = 0;
 let cameraX = 0;
-let speed = 50;
+let speed = 10;
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  frameRate(30);
 }
 
 function draw() {
   background(255);
-  scale(1.5);
+  scale(1);
+  push();
   translate(-cameraX, 0);
-  image(platform, 0, windowHeight/2 -250,0,0,0,-170);
+  image(platform, 0, windowHeight/2 -250);
+  pop();
   scale(0.7)
-  image(Mariomove[currentFrame], cameraX, windowHeight / 2 - 200);
-  currentFrame = currentFrame + 1 % 2;
+  image(Mariomove[currentFrame], 200, windowHeight / 2 - 250);
   
+  currentFrame++
+  if(currentFrame > 2) currentFrame = 0;
+  usercontrol();  
 }
+
 
 function preload(){
   platform = loadImage("Assets/Background.png");
   MarioStand = loadImage("Assets/Mario Stand.png")
 
-  for(let i = 0; i < 2; i++){
+  for(let i = 0; i < 3; i++){
+    Mariomove.push(loadImage("assets/Mariomove-0" + i + ".png"));
+  }
+  for(let i = 0; i < 3; i++){
     Mariomove.push(loadImage("assets/Mariomove-0" + i + ".png"));
   }
 }
-function keyPressed() {
-  if (keyCode === RIGHT_ARROW) {
+function usercontrol() {
+  if (keyIsDown(RIGHT_ARROW)) {
     cameraX += speed;
-    cameraX = constrain(cameraX, 0, platform.width - width);
-  } else if (keyCode === LEFT_ARROW) {
+   
+  } else if (keyIsDown(LEFT_ARROW)) {
     cameraX -= speed;
-    cameraX = constrain(cameraX, 0, platform.width - width);
+    
   }
 }
 
