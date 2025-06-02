c83675fd882175ba6258f94084cf1743cb49da0e
Yao7004906 <145151247+Lastandlast@users.noreply.github.com>
2025-06-02 12:17:08 -0600

    figured out how to jump while i move

--- Final project/sketch.js
+++ Final project/sketch.js
@@ -8,15 +8,18 @@
 let platform;
 let Mariomoveright = [];
 let Mariomoveleft = [];
+let Enemies = [];
 let currentFrame = 0;
 let cameraX = 0;
 let speed = 10;
 let direction = "right"
 
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  y = 200
+  yspeed = 0
   frameRate(30);
 }
 
@@ -28,6 +31,13 @@ function draw() {
   image(platform, 0, windowHeight/2 -250);
   pop();
   scale(0.4)
+  y = y + speed
+  if(y < 200){
+    speed = speed + 1
+  }else{
+    speed = 0
+    y = 200
+  }
   if (direction === "right"){
     image(Mariomoveright[currentFrame], 200, windowHeight / 2 + 550);} 
     else if( direction === "left"){
@@ -35,11 +45,8 @@ function draw() {
       else if(direction === "stand"){
         image(MarioStand,200, windowHeight / 2 + 550);
       }else if( direction === "jump"){
-        image(Mariojump,200, windowHeight / 2 + 500);
+        image(Mariojump,200, y);
       }
-
-  
-  
   
   currentFrame++
   if(currentFrame > 2) currentFrame = 0;
@@ -51,24 +58,32 @@ function preload(){
   platform = loadImage("Assets/Background.png");
   MarioStand = loadImage("Assets/Mario Stand.png");
   Mariojump = loadImage("Assets/Mario jump.png");
+  
 
   for(let i = 0; i < 3; i++){
-    Mariomoveright.push(loadImage("assets/Mariomoveright-0" + i + ".png"));
+    Mariomoveright.push(loadImage("Assets/Mariomoveright-0" + i + ".png"));
   }
   for(let i = 0; i < 3; i++){
-    Mariomoveleft.push(loadImage("assets/Mariomoveleft-0" + i + ".png"));
+    Mariomoveleft.push(loadImage("Assets/Mariomoveleft-0" + i + ".png"));
+  }
+  for(let i = 0 ; i < 2; i++){
+    Enemies.push(loadImage("Assets/Enemies-0" + i + ".png"));
   }
 }
 function usercontrol() {
   if (keyIsDown(RIGHT_ARROW)) {
-    cameraX += speed;
-    direction = "right"
+    cameraX += 10;
+    direction = "right" && "jump"
    
   } else if (keyIsDown(LEFT_ARROW)) {
-    cameraX -= speed;
-    direction = "left"
+    cameraX -= 10;
+    direction = "left" && "jump"
   } else if(keyIsDown(UP_ARROW)){
     direction = "jump"
+    if(y <= 200){
+      speed = -15;
+    }
+    
   }
   else{
     direction = "stand"
